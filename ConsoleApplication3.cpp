233ce0db2f775141dfd8dfd6ec7a7b0e87865c66
Artem Bakhrov <artembahrov00@gmail.com>
2024-11-03 17:19:24 +0200

    files are added

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+#include "math.h"
+#include "windows.h"
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x, y, x1, x2, h,c;
+	cout << "Введіть [x1; x2] та крок h\n";
+	cin >> x1 >> x2 >> h;
+	cout << "****************WHILE****************" << endl;
+	x = x1;
+	while (x <= x2 + h)
+	{
+		y = (log(pow(x, 2)) + pow(x, 2) + 2);
+		cout << setw(10) << "x = " << x << setw(15) << "y = " << y << endl;
+		x += h;
+	}
+	cout << "****************DO_WHILE****************" << endl;
+	x = x1;
+	do
+	{
+		y = (log(pow(x, 2)) + pow(x, 2) + 2);
+		cout << setw(10) << "x = " << x << setw(15) << "y = " << y << endl;
+		x += h;
+	} while (x <= x2 + h);
+	c = sin(27);
+		cout << c;
+}
---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+using namespace std;
+double func_a(double y, double c, double x, double z)
+{
+	return(y != 0 && (x + y) > 0) ? -1 * 4 * z * (pow(pow(x + y, 2) / (y * y), 3) - abs(3 * y + 5 * pow(x, -1))) + exp(3) : 0;
+}
+double func_b(double x, double c, double y,double a, double z)
+{
+	return (x != 0 && (pow(pow(x, 2) + a - pow(y, 2) * pow(sin((x + z) / x), 3), 1 / 2.))>0) ? pow(pow(x, 2) + a - pow(y, 2)*pow(sin((x+z)/x),3),1/2.)+sin((3*M_PI)/20) : 0;
+}
+double func_c(double a, double b)
+{
+	return  a + b;
+}
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x, c, y, a, z;
+	cout << "Введіть x, c, y, a, z" << endl;
+	cin >> x >> c >> y >> a >> z;
+	cout << "A = " << fixed << setprecision(2) << func_a(x, c, y, z) << endl;
+	cout << "B = " << fixed << setprecision(2) << func_b(x, c, y, a, z) << endl;
+	cout << "C = " << fixed << setprecision(2) << func_c(func_a(x, c, y, z), func_b(x, c, y, a, z)) << endl;
+}
