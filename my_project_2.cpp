9fd0cebebb6b64c704d995b4b64157b8f81dc91b
Artem Bakhrov <artembahrov00@gmail.com>
2024-11-26 17:07:43 +0200

    file is changed

--- my_project_2.cpp
+++ my_project_2.cpp
@@ -6,7 +6,7 @@ int main()
 
     cout << setw(14) << showpos << internal << fixed << setprecision(2) << -67.09124 << endl;
     cout << showpos << setw(8) << right << 235 << endl;
-   
+   cout <<endl;
     cout  << right << showpos << setprecision(2) << -121.0 << endl;
     cout << showpos<< dec << +1  << +24 << endl;
     cout << "String1" << '\n'<< '\t' << "String2" << '\n' << setw(23) << "String3" << endl;
