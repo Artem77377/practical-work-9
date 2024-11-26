d1f1dfd32e016e81a589b0cd2d09dc1887384bf7
Artem Bakhrov <artembahrov00@gmail.com>
2024-11-26 16:42:30 +0200

    new file is added and another is changes

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "string.h"
+using namespace std;
+void func(void);
+int main()
+{
+	func();
+}
+void func(void) {
+	char S1[] = "string string";
+	char S2[] = "string56string557766";
+	char *rez; // вказівник для зберігання адреси пам'яті рядка
+	rez = strstr(S2, S1);
+	if (rez == S2)
+		cout << "True";
+	else cout << "False";
+}
